da5113d2ed9e82885c59b3521c9d791a1ca6689b
VikasDewangan <dewanganvikas192@gmail.com>
2024-05-06 12:07:04 +0530

    debugging 1

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const nodemailer = require("nodemailer");
-const port =  process.env.PORT || 5500
+const port =  process.env.PORT || 3000
 const flash = require("connect-flash");
 const session = require("express-session");
 const bodyparser = require("body-parser");
@@ -15,7 +15,7 @@ app.set("port",port);
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,'/public')));
-app.set("views",path.join(__dirname,"/views"));
+app.set("views",path.join(__dirname,"views"));
 app.set("view engine",'ejs');
 app.use(bodyparser.urlencoded({extended:true}));
 
