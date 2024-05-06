385959c579c740932e7bd120c9369103b93c0b8f
VikasDewangan <dewanganvikas192@gmail.com>
2024-05-06 12:15:22 +0530

    debugging 4

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const bodyparser = require("body-parser");
 app.set("port",port);
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,'/public')));
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine",'ejs');
 app.use(bodyparser.urlencoded({extended:true}));
